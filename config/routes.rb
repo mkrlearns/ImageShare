6eeb892adf0994be5926b025a914fe7ae6551461
maykar <25127328+maykar@users.noreply.github.com>
2021-01-01 15:46:30 -0500

    login, logout, & home routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  root 'sessions#home'
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 
   resources :image_tags
   resources :tags
@@ -9,5 +14,5 @@ Rails.application.routes.draw do
   resources :comments
   resources :users
   resources :images
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
