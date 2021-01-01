0eb43d9cc013d8fb5debb522ea0930cc7d51ec4d
maykar <25127328+maykar@users.noreply.github.com>
2021-01-01 15:26:52 -0500

    user signup & validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   has_many :commented_images, through: :comments, source: :image
   has_many :tags, through: :images
   has_secure_password
+
+  validates :username, presence: true, length: { maximum: 20 }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 100 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
