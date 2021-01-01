d45ffd3ae4badace4d8a8ab553fb2da63d4c6dd8
maykar <25127328+maykar@users.noreply.github.com>
2021-01-01 16:43:51 -0500

    session login & cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:id])
-    redirect_to root_path if !@user
+    redirect_to root_path if !@user || @user.id != session[:user_id]
   end
 
   private
